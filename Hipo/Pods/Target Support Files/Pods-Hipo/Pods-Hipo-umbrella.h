82b1706a5f4e1d4a6fb588deb80192d7929b0292
Burak Yılmaz <byilmaz50@hotmail.com>
2020-03-25 05:45:38 +0300

    Bug Fixes

--- Hipo/Pods/Target Support Files/Pods-Hipo/Pods-Hipo-umbrella.h
+++ Hipo/Pods/Target Support Files/Pods-Hipo/Pods-Hipo-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_snapkitTestVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_snapkitTestVersionString[];
+FOUNDATION_EXPORT double Pods_HipoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HipoVersionString[];
 
